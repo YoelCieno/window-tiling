4d2ba7a9d59dca14920c9c277cfe95ee944d44f9
Fer <frmonteira@gmail.com>
2026-05-10 21:34:40 +0200

    feat(startup): add .gitignore and complete tiling scripts

--- startup/startup-dev.sh
+++ startup/startup-dev.sh
@@ -13,5 +13,5 @@ obsidian &
 # ── Place windows (workspace 0 = ws1, workspace 1 = ws2) ────
 place "firefox" 0 0 $W_4_5 $SH 0
 place "obsidian" $W_2_3 0 $W_1_3 $SH 0
-place "dev.zed.Zed" $((SW - W_4_5)) 0 $W_4_5 $SH 1
+place "dev.zed.Zed" 0 0 $W_4_5 $SH 1
 place "io.elementary.terminal" $W_2_3 0 $W_1_3 $SH 1
